83721fb626f39b41c7dc6e219290d6ae80688afc
El <elmoctarebnou@gmail.com>
2022-11-15 17:25:01 -0700

    Small change

--- tmux-session-setup.sh
+++ tmux-session-setup.sh
@@ -5,17 +5,12 @@ SESSION='el-dev'
 if [ "$(tmux list-sessions 2> /dev/null | grep -o $SESSION)" != "$SESSION" ]; then
   # Create session 
   tmux new-session -d -s $SESSION
-  # Create windows
-  tmux unlink-window -k -t $SESSION:1
-  tmux new-window -t $SESSION:1 -n 'frontend' "$SHELL"
+  # Rename auto created window and create other windows 
+  tmux rename-window -t $SESSION:1 'frontend'
   tmux new-window -t $SESSION:2 -n 'backend' "$SHELL"
   tmux new-window -t $SESSION:3 -n 'aws' "$SHELL"
   tmux new-window -t $SESSION:4 -n 'notes' "$SHELL"
   tmux new-window -t $SESSION:5 -n 'other' "$SHELL"
- 
-  # Gets rid of window 0, which is not accessible right away (hence sleep 1)
-  sleep 1
-  tmux unlink-window -k -t $SESSION:0
  
 else
   tmux attach -t $SESSION
